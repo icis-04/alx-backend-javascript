aed9ef5a8d7ac393d4a7f333c17ae680c0cfe7df
icis-04 <chidexiwuaku@gmail.com>
2022-12-22 04:25:55 +0000

    updated then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
     const body = { status: 200, body: 'success' };
 
     return promise
-        .then(() => body);
-        .catch(() => new Error);
+        .then(() => body)
+        .catch(() => new Error)
         .finally(() => {
-            console.log('Got a response from the API');
+            console.log('Got a response from the API')
         });
 
 }
