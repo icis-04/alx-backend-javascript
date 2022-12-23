926f24d7f97f5d98ec5eaaa4155206fb5c4fa95e
icis-04 <chidexiwuaku@gmail.com>
2022-12-23 04:10:49 +0000

    classrooms file updated

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+    return new Array(
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(24));
+}
