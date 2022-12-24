de9610bffd5490588f4bf69dadc1fe226c925a24
icis-04 <chidexiwuaku@gmail.com>
2022-12-24 22:13:48 +0000

    implemented eslint formatting

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (let idx of array) {
-        const value = array.indexOf(idx);
-        array[value] = appendString + idx;
-    }
+  for (const idx of array) {
+    const value = array.indexOf(idx);
+    array[value] = appendString + idx;
+  }
 
-    return array;
+  return array;
 }
