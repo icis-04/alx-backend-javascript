ac27d9a230b6b199ff67a617dc3c0ebf306632b9
icis-04 <chidexiwuaku@gmail.com>
2023-02-21 18:25:42 +0000

    node stuffs commited

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', function () {
+	let chunk = process.stdin.read();
+	if (chunk !== null) {
+		process.stdout.write('Your name is: ' + chunk)
+	}
+});
+
+process.stdin.on('end', function() {
+	process.stdout.write('This important software is now closing\n');
+});
