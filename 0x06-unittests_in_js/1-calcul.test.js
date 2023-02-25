b6e1925d87578995a4c2e491098f17566fcf9eb1
icis-04 <chidexiwuaku@gmail.com>
2023-02-25 04:46:22 +0000

    test cases added

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,17 +1,45 @@
 const calculateNumber = require("./1-calcul.js");
 const assert = require("assert");
 
-describe("calculateNumber", function() {
+describe("calculateNumber - SUM", function() {
 	it("tetsing functions based on types", function(){
 		assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
 	});
 	it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUM', -5, 4.5), 0);
+        });
+	it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUM', 7, 8), 15);
+        });
+	it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUM', 1.4, 0), 1);
+        });
+})
+describe("calculateNumber - SUBTRACT", function() {
+        it("tetsing functions based on types", function(){
                 assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
         });
-	it("tetsing functions based on types", function(){
+        it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUBTRACT', 1.4, -1.4), 2);
+        });
+        it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUBTRACT', -1.4, -3.7), 3);
+        });
+        it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('SUBTRACT', 12, 6), 6);
+        });
+})
+describe("calculateNumber", function() {
+        it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('DIVIDE', -9, 4.5), -1.8);
+        });
+        it("tetsing functions based on types", function(){
+                assert.equal(calculateNumber('DIVIDE', 0, 4.5), 0);
+        });
+        it("tetsing functions based on types", function(){
                 assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
         });
-	it("tetsing functions based on types", function(){
+        it("tetsing functions based on types", function(){
                 assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
         });
 })
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,14 +1,18 @@
-const calculateNumber = require("./1-calcul.js");
+const calculateNumber = require("./2-calcul_chai.js");
 const chai = require("chai");
 const expect = chai.expect;
 
-describe("calculateNumber", function() {
+describe("calculateNumber ", function() {
         it("tetsing functions based on types", function(){
                 expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6);
         });
+});
+describe("calculateNumber ", function() {
         it("tetsing functions based on types", function(){
                 expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4);
         });
+});
+describe("calculateNumber ", function() {
 	it("tetsing functions based on types", function(){
                 expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2);
         });
@@ -16,4 +20,4 @@ describe("calculateNumber", function() {
                 expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('Error');
         });
 
-})
+});
